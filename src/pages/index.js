52c686254df36b09292c8f585211228181cbf1b4
sidhantmathur <sidhant185@gmail.com>
2021-02-10 18:36:55 -0600

    add props to pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Header from "../components/header"
 export default function Home() {
   return (
     <div style={{ color: `purple` }}>
-      <Header />
+      <Header headerText="Gatsby" />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
