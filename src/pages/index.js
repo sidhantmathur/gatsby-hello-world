26f3654cb9ae425b2723ea0f8be41f56ec3e03ee
sidhantmathur <sidhant185@gmail.com>
2021-02-10 18:31:04 -0600

    add header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Header from "../components/header"
 
 export default function Home() {
   return (
     <div style={{ color: `purple` }}>
-      <h1>Hello Gatsby!</h1>
+      <Header />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
